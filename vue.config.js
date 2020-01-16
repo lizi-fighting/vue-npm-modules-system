c7beef6361e222f573547851273ffb22a49ecb38
徐兴丽 <xuxingli@cindata.cn>
2020-01-16 17:52:51 +0800

    package: vue-npm-modules-system@0.1.0

--- vue.config.js
+++ vue.config.js
@@ -1,32 +1,3 @@
-const path = require('path')
-
-// If your port is set to 80,
-// use administrator privileges to execute the command line.
-// For example, on Mac: sudo npm run / sudo yarn
-const devServerPort = 9527 // TODO: get this variable from setting.ts
-const mockServerPort = 9528 // TODO: get this variable from setting.ts
-const name = process.env.VUE_APP_SYSTEM_TITLE // TODO: get this variable from setting.ts
-
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/basic/' : '/',
-  lintOnSave: process.env.NODE_ENV === 'development',
-  productionSourceMap: false,
-  devServer: {
-    port: devServerPort,
-    open: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    proxy: {
-      [process.env.VUE_APP_BASE_API]: {
-        target: `http://10.201.71.180:9030`,
-        changeOrigin: true, // needed for virtual hosted sites
-        ws: true, // proxy websockets
-        pathRewrite: {
-          ['^' + process.env.VUE_APP_BASE_API]: ''
-        }
-      }
-    }
-  }
+  css: { extract: false }
 }
